b2e8f400f0701d6dceeb9939b8cd109ab06697f5
ianlizzo <ianliz22@icloud.com>
2019-02-18 03:00:16 -0800

    started chatserver.py

--- homework1/python/chat_server.py
+++ homework1/python/chat_server.py
@@ -1,1 +1,10 @@
 import socketserver
+import threading
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+if __name__ == '__main__':
+    server = ThreadedTCPServer(('', 59001), )
