4efd2ac4ad95248c45f240518805e7595a4297c7
ianlizzo <ianliz22@icloud.com>
2019-02-19 14:53:49 -0800

    uhh more stuff

--- homework1/python/chat_server.py
+++ homework1/python/chat_server.py
@@ -6,5 +6,12 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 
+class ChatHandler():
+    pass
+
+
 if __name__ == '__main__':
-    server = ThreadedTCPServer(('', 59001), )
+    server = ThreadedTCPServer(('', 59001), ChatHandler)
+    with server:
+        print(f'The chat server is running...')
+        server.serve_forever()
