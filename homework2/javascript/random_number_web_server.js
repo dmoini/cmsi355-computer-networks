90e4d5c0d582ea7bd273098079b9206abc3f15cf
ianlizzo <ianliz22@icloud.com>
2019-03-24 20:38:37 -0700

    1-3 done for JS applications

--- homework2/javascript/random_number_web_server.js
+++ homework2/javascript/random_number_web_server.js
@@ -1,23 +1,22 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 server.listen(53211);
 // WARNING: app.listen(80) will NOT work here!
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/random', function (req, res) {
+app.get("/random", function(req, res) {
   let random = Math.floor(Math.random() * 100) + 1;
   res.send(`${random}`);
-})
+});
 
-io.on('connection', function (socket) {
-  let random = Math.floor(Math.random() * 100) + 1;
-  socket.emit('news', random);
-  socket.on('my other event', function (data) {
+io.on("connection", function(socket) {
+  socket.emit("random", "Hello client!");
+  socket.on("Connected", function(data) {
     console.log(data);
   });
 });
