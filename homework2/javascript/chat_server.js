42f6831e85cfd978003240532971be01c29a2ad1
Donovan Moini <donovanmoini@gmail.com>
2019-03-27 17:55:01 -0700

    WIP

--- homework2/javascript/chat_server.js
+++ homework2/javascript/chat_server.js
@@ -2,12 +2,21 @@ const WebSocket = require("ws");
 const http = require("http");
 const server = new WebSocket.Server({ port: 58901 });
 
-let name = Set();
-let writers = Set();
+let name = new Set();
+let writers = new Set();
+
+server.broadcast = function broadcast(data) {
+  server.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
 (() => {
   server.on("connection", (socket, req) => {
     console.log("Connection from", req.connection.remoteAddress);
     writers.push(socket);
   });
-  console.log("The Chat server is running...");
+  console.log("The chat server is running...");
 })();
