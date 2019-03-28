680fa2b716a2a935533cc314a8ba952e60136666
ianlizzo <ianliz22@icloud.com>
2019-03-28 02:52:04 -0700

    HEHEHEHEH

--- homework2/javascript/chat_server.js
+++ homework2/javascript/chat_server.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
   // res.send('<h1>IAN SUCKS</h1>')
 });
 
-const users = [];
+let users = [];
 
 io.on("connection", socket => {
   console.log("A new user has joined the chat!");
@@ -20,28 +20,22 @@ io.on("connection", socket => {
     console.log("username received");
     console.log(data);
     if (users.includes(data)) {
-      io.emit("taken username", true);
+      socket.emit("taken username", true);
       console.log("username taken");
     } else {
       console.log("username accepted");
-      io.emit("taken username", false);
+      socket.emit("taken username", false);
       users.push(data);
       socket.username = data;
       console.log(users);
     }
   });
-
-  // socket.on('message', data => {
-  //   console.log('data' + data);
-  //   io.sockets.emit('message', {
-  //     username: socket.username,
-  //     message: data.message,
-  //   });
-  // });
   socket.on("message", msg => {
     io.emit("message", msg);
   });
   socket.on("disconnect", () => {
+    users = users.filter(e => e !== socket.username);
     console.log("A user has disconnected. :(");
+    console.log(users);
   });
 });
