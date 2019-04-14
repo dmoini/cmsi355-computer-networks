4e593e26ad13472ad52e5785386a3e2d2bb7d584
Donovan Moini <donovanmoini@gmail.com>
2019-04-14 15:25:30 -0700

    Fixed binary_to_colon_hex zero compression

--- homework3/binary_to_colon_hex.py
+++ homework3/binary_to_colon_hex.py
@@ -1,26 +1,41 @@
 # b is a 128=bit binary number
 def binary_to_colon_hex(b):
-    colon_hex = ''
-    for i in range(0, 128, 8):
-        eight_bits = b[i:i + 8]
-        hex_chunk = hex(int(eight_bits,2))[2:].upper()
-        if colon_hex and colon_hex[-1] == '0' and hex_chunk == '0':
-            colon_hex += ':' if (i % 16 != 0 and i < 120) else ''
-            continue
-        if hex_chunk != '0' and len(hex_chunk) == 1:
-            hex_chunk = '0' + hex_chunk
-        colon_hex += hex_chunk + (':' if (i % 16 != 0 and i < 120) else '')
-    return zero_compression(colon_hex)
+    MAX_BITS = 128
+    BITS_CHUNK = 16
+    hex_chunks = ['' for i in range(8)]
+    for i in range(0, MAX_BITS, BITS_CHUNK):
+        sixteen_bits = b[i:i + BITS_CHUNK]
+        hex_chunk = hex(int(sixteen_bits,2))[2:].upper()
+        hex_chunks[i // 16] = leading_zero_compression(hex_chunk)
+    return zero_compression(hex_chunks)
 
-def zero_compression(ch):
+def leading_zero_compression(hc):
+    if hc == '0':
+        return hc
+    non_zero_index = 0
+    while hc[non_zero_index] == '0' and non_zero_index < 3:
+        non_zero_index += 1
+    return hc[non_zero_index:]
+    
+def zero_compression(hc):
     found_sequence_of_zeroes = False
-    zero_compressed_colon_hex = ''
-    for i in range(0, len(ch)):
-        if i > 0 and i < len(ch) - 1 and ch[i - 1] == ':' and ch[i] == '0' and ch[i + 1] == ':':
+    zero_sequences_indexes = []
+    si, ei = 0, 0  #start and end index
+    for i, c in enumerate(hc):
+        if c == '0' and not found_sequence_of_zeroes:
+            si = i
             found_sequence_of_zeroes = True
-        if found_sequence_of_zeroes and ch[i] != '0' and ch[i] != ':':
+        elif c != '0' and found_sequence_of_zeroes:
+            ei = i
             found_sequence_of_zeroes = False
-            zero_compressed_colon_hex += ':'
-        if not found_sequence_of_zeroes:
-            zero_compressed_colon_hex += ch[i]
-    return zero_compressed_colon_hex
+            zero_sequences_indexes.append((si, ei))
+    zci = max(zero_sequences_indexes, key=max_range)  #zci = zero compression indexes
+    new_hc = hc[:zci[0] + 1] + hc[zci[1]:]
+    new_hc[zci[0]] = ''
+    new_hc = ':'.join(new_hc)
+    if new_hc[0] == ':':
+        new_hc = ':' + new_hc
+    return new_hc
+
+def max_range(a):
+    return abs(a[0] - a[1])
