4249cad1311627ae6bce7a8fed17caa501076fb0
Donovan Moini <donovanmoini@gmail.com>
2019-04-29 00:02:29 -0700

    WIP

--- homework3/littlektah/app.js
+++ homework3/littlektah/app.js
@@ -13,27 +13,35 @@ app.get('/', (req, res) => {
 });
 app.listen(50000, () => console.log("Little K'tah server is running"));
 
-const state = new Map();
-
-// app.get("/game", function(req, res) {
-//   let random = Math.floor(Math.random() * 100) + 1;
-//   res.send(`${random}`);
-// });
-
 // TODO: fix issue where server crashes when use reloads page
 
 function randomColor() {
   const [r, g, b] = Array(3).fill(0).map(() => Math.floor(Math.random() * 200));
   return `rgb(${r}, ${g}, ${b})`;
-}
+};
+
+const state = new Map();
+const usernames = new Set();
 
 new WebSocket.Server({ port: 50001 }).on('connection', (socket) => {
+  console.log('Connection from socket');
 
   state.set(socket, { location: [0, 0], color: randomColor() });
   socket.on('message', (data) => {
+    console.log(`RECEIVED MESSAGE: ${data}`);
     state.get(socket).location = JSON.parse(data);
     const renderData = JSON.stringify(Array.from(state.values()));
     Array.from(state.keys()).forEach(sock => sock.send(renderData));
+  });
+
+  socket.on('submitted username', (username) => {
+    console.log(`RECEIVED USERNAME: ${username}`);
+    if (usernames.has(username)) {
+      socket.emit('username-taken');
+    } else {
+      usernames.add(username);
+      socket.emit('welcome')
+    }
   });
 
   socket.on('error', (e)=> {
--- homework3/littlektah/public/littlektah.js
+++ homework3/littlektah/public/littlektah.js
@@ -1,13 +1,27 @@
+const usernameFormInput = $('#username-input')
+const usernameFormButton = $('#username-button');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+let username = '';
 
 const socket = new WebSocket(`ws://${location.hostname}:50001`);
 
-canvas.addEventListener('mousemove', (e) => {
-  const rect = canvas.getBoundingClientRect();
-  const [x, y] = [e.clientX - rect.left, e.clientY - rect.top];
-  socket.send(JSON.stringify([x, y]));
+
+usernameFormButton.click(() => {
+  console.log('BUTTON CLICKED');
+  username = usernameFormInput.val().toLowerCase();
+  if (username) {
+    console.log(`USERNAME: ${username}`);
+    // socket.emit('submitted username', username);
+    socket.send(JSON.stringify(username));
+  }
 });
+
+socket.addEventListener('username-taken', () => {
+  // usernameFormInput.val('');
+  $('#taken-username').removeAttr('display');
+});
+
 
 socket.addEventListener('message', (event) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -17,9 +31,15 @@ socket.addEventListener('message', (event) => {
   });
 });
 
+canvas.addEventListener('mousemove', (e) => {
+  const rect = canvas.getBoundingClientRect();
+  const [x, y] = [e.clientX - rect.left, e.clientY - rect.top];
+  socket.send(JSON.stringify([x, y]));
+});
+
 // TODO: update to current jQuery
 // When the server sends us the `welcome` message, hide the lobby for and show the game board.
-socket.on('welcome', () => {
+socket.addEventListener('welcome', () => {
   $('#login').style.display = 'none';
   $('#game').style.display = 'block';
 });
