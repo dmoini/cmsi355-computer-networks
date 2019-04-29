5125730e3f0557badf206d07848f2aa02133504e
Donovan Moini <donovanmoini@gmail.com>
2019-04-29 01:15:14 -0700

    WIP

--- homework3/littlektah/app.js
+++ homework3/littlektah/app.js
@@ -1,17 +1,22 @@
-const path = require('path');
-const express = require('express');
-const WebSocket = require('ws');
-
+// const path = require("path");
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
-app.get('/', (req, res) => {
+const WebSocket = require("ws");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+server.listen(50000, () => console.log("Little K'tah server is running on port 50000"));
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
   // TODO: check for unique username
-  res.sendFile(path.join(__dirname, 'public', 'littlektah.html'));
+  // res.sendFile(path.join(__dirname, "public", "littlektah.html"));
+  res.sendFile(__dirname + "/public/littlektah.html");
 
   // If unique username, then send to game
-  // res.sendFile(path.join(__dirname, 'public', 'littlektah.html'))
+  // res.sendFile(path.join(__dirname, "public", "littlektah.html"))
 });
-app.listen(50000, () => console.log("Little K'tah server is running"));
+// app.listen(50000, () => console.log("Little K'tah server is running"));
 
 // TODO: fix issue where server crashes when use reloads page
 
@@ -23,28 +28,33 @@ function randomColor() {
 const state = new Map();
 const usernames = new Set();
 
-new WebSocket.Server({ port: 50001 }).on('connection', (socket) => {
-  console.log('Connection from socket');
+// new WebSocket.Server({ port: 50001 }).on("connection", (socket) => {
+io.on("connection", (socket) => {
+  console.log("New connection");
 
   state.set(socket, { location: [0, 0], color: randomColor() });
-  socket.on('message', (data) => {
+  // TODO: change event from "message" to something else
+  socket.on("message", (data) => {
     console.log(`RECEIVED MESSAGE: ${data}`);
     state.get(socket).location = JSON.parse(data);
     const renderData = JSON.stringify(Array.from(state.values()));
     Array.from(state.keys()).forEach(sock => sock.send(renderData));
   });
 
-  socket.on('submitted username', (username) => {
+  socket.on("submitted username", (username) => {
     console.log(`RECEIVED USERNAME: ${username}`);
     if (usernames.has(username)) {
-      socket.emit('username-taken');
+      console.log("SERVER Username taken")
+      socket.emit("username-taken");
     } else {
+      console.log("SERVER Untaken username")
       usernames.add(username);
-      socket.emit('welcome')
+      socket.emit("accepted username")
     }
+    console.log("USERNAMES:", usernames);
   });
 
-  socket.on('error', (e)=> {
+  socket.on("error", (e)=> {
     console.log(e);
   })
 });
--- homework3/littlektah/public/littlektah.js
+++ homework3/littlektah/public/littlektah.js
@@ -1,29 +1,50 @@
-const usernameFormInput = $('#username-input')
-const usernameFormButton = $('#username-button');
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-let username = '';
+let socket = io();
+const usernameInstructions = $("#username-instructions");
+const usernameFormInput = $("#username-input")
+const usernameFormButton = $("#username-button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+let username = "";
 
-const socket = new WebSocket(`ws://${location.hostname}:50001`);
-
+// const socket = new WebSocket(`ws://${location.hostname}:50001`);
+// const socket = new io.Socket("localhost",{ port: 50001 });
+// socket.connect();
 
 usernameFormButton.click(() => {
-  console.log('BUTTON CLICKED');
+  console.log("BUTTON CLICKED");
   username = usernameFormInput.val().toLowerCase();
   if (username) {
     console.log(`USERNAME: ${username}`);
-    // socket.emit('submitted username', username);
-    socket.send(JSON.stringify(username));
+    // socket.emit("submitted username", username);
+    // socket.send(JSON.stringify(username, username));
+    socket.emit("submitted username", username);
   }
 });
 
-socket.addEventListener('username-taken', () => {
-  // usernameFormInput.val('');
-  $('#taken-username').removeAttr('display');
+usernameFormInput.keypress((e) => {
+  let key = e.which;
+  if(key == 13) {  // the enter key code
+     usernameFormButton.click();
+     return false;  
+   }
+ });
+
+let fontSize = 20;
+socket.on("username-taken", () => {
+  // usernameFormInput.val("");
+  console.log("CLIENT username taken")
+  // console.log($("#taken-username").text());
+  // console.log(JSON.stringify($("#taken-username")));
+  usernameInstructions.addClass("taken-username");
+  usernameInstructions.css("font-size", fontSize + "px");
+  if (fontSize < 33) {
+    fontSize += 1;
+  }
+  // console.log(`FONT SIZE: ${fontSize}`);
 });
 
 
-socket.addEventListener('message', (event) => {
+socket.on("message", (event) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   JSON.parse(event.data).forEach(({ location, color }) => {
     ctx.fillStyle = color;
@@ -31,15 +52,22 @@ socket.addEventListener('message', (event) => {
   });
 });
 
-canvas.addEventListener('mousemove', (e) => {
+canvas.addEventListener("mousemove", (e) => {
+  console.log("MOUSEMOVE1");
   const rect = canvas.getBoundingClientRect();
+  console.log("MOUSEMOVE2");
   const [x, y] = [e.clientX - rect.left, e.clientY - rect.top];
-  socket.send(JSON.stringify([x, y]));
+  console.log("MOUSEMOVE3");
+  socket.emit("message", JSON.stringify([x, y]));
+  console.log("MOUSEMOVE4");
 });
 
 // TODO: update to current jQuery
 // When the server sends us the `welcome` message, hide the lobby for and show the game board.
-socket.addEventListener('welcome', () => {
-  $('#login').style.display = 'none';
-  $('#game').style.display = 'block';
+socket.on("accepted username", () => {
+  console.log("CLIENT accepted username")
+  // $("#login").css("display", "none");
+  // $("#game").css("display", "block");
+  $("#login").hide()
+  $("#game").show();
 });
